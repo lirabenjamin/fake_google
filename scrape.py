d630ea68621583a8efe9617851d920b3f3b0f02f
“Benjamin <lira.benjamin@gmail.com>
2025-07-19 10:47:09 -0400

    Implement code changes to enhance functionality and improve performance

--- scrape.py
+++ scrape.py
@@ -1,15 +1,18 @@
 from playwright.sync_api import sync_playwright
 
+URL_TO_SCRAPE = "https://www.quora.com/What-is-the-best-cover-letter-you-have-ever-read-or-written"
+PLACE_TO_SAVE = "scraped_version.html"
+
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-    page.goto("https://www.canva.com/letters/templates/cover-letters/")
+    page.goto(URL_TO_SCRAPE)
     
 
     # Get the full rendered HTML
     html = page.content()
     
-    with open("canva.html", "w", encoding="utf-8") as f:
+    with open(PLACE_TO_SAVE, "w", encoding="utf-8") as f:
         f.write(html)
     
     browser.close()
